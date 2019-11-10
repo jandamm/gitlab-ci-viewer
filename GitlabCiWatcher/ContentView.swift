340babd26e81f8be604803d39fb609b47ec793e7
Jan Dammshäuser <mail@jandamm.de>
2019-11-10 20:20:14 +0100

    Initial commit

---
+++ GitlabCiWatcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitlabCiWatcher
+//
+//  Created by Jan Dammshäuser on 10.11.19.
+//  Copyright © 2019 Jan Dammshäuser. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitlabCiWatcherTests/GitlabCiWatcherTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitlabCiWatcherTests.swift
+//  GitlabCiWatcherTests
+//
+//  Created by Jan Dammshäuser on 10.11.19.
+//  Copyright © 2019 Jan Dammshäuser. All rights reserved.
+//
+
+import XCTest
+@testable import GitlabCiWatcher
+
+class GitlabCiWatcherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
